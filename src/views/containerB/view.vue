f3d608b039199999406bff5402f2f5ef07e7f7f4
SugarTeam <13922793572@163.com>
2018-10-31 21:15:54 +0800

    newfiles

---
+++ src/views/containerB/view.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <div>容器B属性：{{containerBAttr}}</div>
+      <div>私有属性：{{priveAttr}}</div>
+      <button @click="onUpdate">更新容器组件B</button>
+  </div>
+</template>
+<script>
+
+export default {
+    name: 'viewB',
+    props: {
+        containerBAttr: {
+            type: String,
+            default: ''
+        },
+        priveAttr: {
+            type: String,
+            default: ''
+        }
+    },
+    methods: {
+        onUpdate() {
+            this.$emit('on-update');
+        }
+    }
+}
+</script>
+
+
